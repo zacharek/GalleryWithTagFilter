7d825315d367e9cf7c2c0662107271042c4d6231
Szymon.Zacharewicz <szymon.zacharewicz@gmail.com>
2019-11-11 22:31:21 +0100

    added filtering script

--- js/app.js
+++ js/app.js
@@ -1,38 +1,34 @@
-let imgs = document.querySelectorAll("img");
-let showButton = document.querySelector("#showButton");
-let hideButton = document.querySelector("#hideButton");
-let tagInput = document.querySelector("#tagInput");
-let clearFilter = document.querySelector('#clearFilter');
-
-/*
-console.log(imgs)
-
-showButton.addEventListener('click', function(){
-    let inputValue = tagInput.value;
-    imgs.forEach(function(img) {
-        if(img.dataset.tag.split(',').indexOf(inputValue) != -1){
-            img.classList.remove('invisible')
-        }else{
-            img.classList.add('invisible');
-        }
-    })
-    tagInput.value = '';
-})
-
-hideButton.addEventListener('click', function(){
-    let inputValue = input.value;
-    imgs.forEach(function(img) {
-        if(img.dataset.tag.split(',').indexOf(inputValue) != -1){
-            img.classList.add('invisible')
-        }else{
+document.addEventListener("DOMContentLoaded", function () {
+    let imgs = document.querySelectorAll('img');
+    let hideBtn = document.querySelector('#hideButton');
+    let showBtn = document.querySelector('#showButton');
+    let tagInput = document.querySelector('#tagInput');
+    let clearBtn = document.querySelector('#clearButton');
+    showBtn.addEventListener('click', function () {
+        let inputValue = tagInput.value;
+        imgs.forEach(function(img) {
+            if (img.dataset.tag.split(',').indexOf(inputValue) !== -1){
+                img.classList.remove('invisible');
+            }else{
+                img.classList.add('invisible');
+            }
+        });
+        tagInput.value = '';
+    });
+    hideBtn.addEventListener('click', function () {
+        let inputValue = tagInput.value;
+        imgs.forEach(function(img) {
+            if (img.dataset.tag.split(',').indexOf(inputValue) !== -1){
+                img.classList.add('invisible');
+            }else{
+                img.classList.remove('invisible');
+            }
+        })
+        tagInput.value = '';
+    });
+    clearBtn.addEventListener('click', function (e) {
+        imgs.forEach(function (img) {
             img.classList.remove('invisible');
-        }
+        })
     })
-    tagInput.value = '';
-})
-
-clearFilter.addEventListener('click', function(){
-    imgs.forEach(function (img) {
-        img.classList.remove('invisible');
-    })
-})*/
+});
